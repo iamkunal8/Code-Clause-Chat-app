78dbd397af38a9b2baf6dbaf61d934e792057467
iamkunal8 <145126800+iamkunal8@users.noreply.github.com>
2023-09-26 10:39:31 +0530

    Add files via upload
    ghdg

--- index.js
+++ index.js
@@ -1,134 +1,65 @@
-'use strict';
+"use strict";
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const fs = require('fs');
 
-let Fake, i=0, my_username = '';	
-const socket = io();		
+app.use('/public', express.static('public')) 
 
-
-$(document).ready( () => {	
-	new MainChat();
+app.get('/', (req, res) =>  {
+  res.sendFile(__dirname + '/'); 
 });
 
+let usernames = {};
 
-// A base class is defined using the new reserved 'class' keyword
-class MainChat  {
-	constructor () {
-		$('.messages-content').mCustomScrollbar();
-		MainChat.LoadEventHandlers();		
-	}	
-	
-	static setDate(){
-	  let d = new Date();
-	  let m = d.getMinutes();
-	  $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+const check_key = v =>{
+	let val = '';	
+	for(let key in usernames){
+		if(usernames[key] == v)	val = key;
 	}
-	
-	static insertMessage() {
-	  const msg = $('.message-input').val();
-	  if ($.trim(msg) == '') return false;
-	  $('.message-input').val(null);
-	  MainChat.updateScrollbar();
-	  // tell server to execute 'sendchat' and send along one parameter
-	  socket.emit('sendchat', msg);
-	}
-		
-	static LoadEventHandlers() {
-			Fake = [ 'Hi there, I\'m Kunal and you?', 'Nice to meet you', 'How are you?', 'Not too bad, thanks', 'What do you do?', 'That\'s awesome', 'Codepen is a nice place to stay', 'I think you\'re a nice person', 'Why do you think that?', 'Can you explain?', 'Anyway I\'ve gotta go now', 'It was a pleasure chat with you', 'Time to make a new codepen', 'Bye', ':)' ]
-
-			$('.chat-title').on('click', () => {
-				if ( $('.chat').height() == 500 )
-					$( '.chat' ).animate({ height: 45 }, 1000 );
-				else 
-					$( '.chat' ).animate({ height: 500 }, 1000 );
-				
-				$('.message-box').slideToggle(300, 'swing');
-				$('.chat-message-counter').fadeToggle(300, 'swing');
-			});
-			
-			$('.message-submit').click( () => {
-			  MainChat.insertMessage();
-			});
-			
-			$(window).on('keydown', e => {
-			  if (e.which == 13) {
-				MainChat.insertMessage();
-				return false;
-			  }
-			});
-			
-			
-			// listener, whenever the server emits 'updatechat', this updates the chat body
-			socket.on('updatechat',  (username, data) => {
-				if(username == 'Chat Bot'){
-					$('<div class="message loading new"><figure class="avatar"><img src="/public/img/sa.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
-					MainChat.updateScrollbar();
-					setTimeout( () => {
-					$('.message.loading').remove();
-					$('<div class="message new"><figure class="avatar"><img src="/public/img/sa.png" /></figure>' + data + '</div>').appendTo($('.mCSB_container')).addClass('new');
-						MainChat.setDate();
-						MainChat.updateScrollbar();
-					}, 1000 + (Math.random() * 20) * 100);
-				} else {	
-					setTimeout( () => {
-					$('.message.loading').remove();
-					$('<div class="message message-personal">' + data + '</div>').appendTo($('.mCSB_container')).addClass('new');
-						MainChat.setDate();
-						MainChat.updateScrollbar();
-					}, 100);
-					
-					setTimeout( () =>	MainChat.fakeMessage(), 1000 + (Math.random() * 20) * 100);
-				}
-			});
-			
-			// listener, whenever the server emits 'msg_user_handle', this updates the chat body
-			socket.on('msg_user_handle', (username, data) => {
-				setTimeout( () => {
-				$('.message.loading').remove();
-				$('<div class="message message-personal">' + data + '</div>').appendTo($('.mCSB_container')).addClass('new');
-					MainChat.setDate();
-					MainChat.updateScrollbar();
-				}, 1000 + (Math.random() * 20) * 100);
-			});
-			
-			// on connection to server, ask for user's name with an anonymous callback
-			socket.on('connect', () => socket.emit('adduser', prompt("What's your name?")));
-			
-			socket.on('msg_user_found', username => socket.emit('msg_user', username, my_username, prompt("Type your message:")));
-			
-			socket.on('store_username', username => my_username = username);
-	}
-	
-	static updateScrollbar() {
-		$('.messages-content').mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
-			scrollInertia: 10,timeout: 0
-		});
-	}
-	
-	// when the user sends a private msg to a user id, first find the username
-	static sendmsg (id) { 
-		socket.emit('check_user', my_username, id);
-	}
-	
-	static fakeMessage() {
-		if ($('.message-input').val() != '') return false;
-		$('<div class="message loading new"><figure class="avatar"><img src="/public/img/sa.png" /></figure><span></span></div>').appendTo($('.mCSB_container'));
-			MainChat.updateScrollbar();
-			setTimeout( () => {
-					$('.message.loading').remove();
-					$('<div class="message new"><figure class="avatar"><img src="/public/img/sa.png" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
-						MainChat.setDate();
-						MainChat.updateScrollbar();
-						i++;
-			}, 1000 + (Math.random() * 20) * 100);
-	}
-	
+	return val;
 }
 
+io.on('connection',  socket => {es
+	socket.on('sendchat', data => io.emit('updatechat', socket.username, data));
 
 
+	socket.on('adduser', username => {
+		socket.username = username;
+		usernames[username] = socket.id;
+		socket.emit('updatechat', 'Chat Bot', `${socket.username} you have joined the chat`);
+		// echo to client their username
+		socket.emit('store_username', username);
+		// echo globally (all clients) that a person has connected
+		//socket.broadcast.emit('updatechat', 'Chat Bot', `${username} has connected`);
+	});
+
+	// when the user disconnects.. perform this
+	socket.on('disconnect', () => {
+		// remove the username from global usernames list
+		delete usernames[socket.username];
+		// echo globally that this client has left
+		//socket.broadcast.emit('updatechat', 'Chat Bot', `${socket.username} has left chat`);
+	});
+	
+	// when the user sends a private msg to a user id, first find the username
+	socket.on('check_user', (asker, id) => io.to(usernames[asker]).emit('msg_user_found', check_key(id)));
+	
+	// when the user sends a private message to a user.. perform this
+	socket.on('msg_user', (to_user, from_user, msg) => {
+		//emits 'msg_user_handle', this updates the chat body on client-side
+		io.to(usernames[to_user]).emit('msg_user_handle', from_user, msg);
+		//write the chat message to a txt file		
+		const wstream = fs.createWriteStream('chat_data.txt');		
+		wstream.write(msg);
+		wstream.write('\r\n');
+		wstream.end();
+		
+	});
 
 
+});
 
-
-
-
-
+http.listen(3000, () => console.log('listening on *:3000'));
+    
